1d5c2b5310ede1d6ff741a513e1fe8f28faa5cdb
ZahidInGit <it24012@mbstu.ac.bd>
2026-04-12 23:42:03 +0600

    Update Employee.java

--- OOP course modules/Module 1/Build your own constructor/Employee.java
+++ OOP course modules/Module 1/Build your own constructor/Employee.java
@@ -1,1 +1,29 @@
-..
+public class Employee {
+    private String name;
+    private int potentialPoints;
+    private double salary;
+
+    public Employee(String name, int potentialPoints, double salary) {
+        this.name = name;
+        this.potentialPoints = potentialPoints;
+        this.salary = salary;
+    }
+  
+    public void applyPromotion() {
+        double raiseAmount = this.salary * 0.15; // 15% raise
+        this.salary += raiseAmount;
+        System.out.println("✅ PROMOTED! " + this.name + " gets the promotion. New salary: $" + this.salary + "\n");
+    }
+  
+    public String getName() {
+        return name;
+    }
+
+    public int getPotentialPoints() {
+        return potentialPoints;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+}
