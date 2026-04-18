33ab66104e0517ab89f1a0120b83d125d87b927c
ZahidInGit <it24012@mbstu.ac.bd>
2026-04-18 20:41:29 +0600

    Create Main.java

---
+++ OOP course modules/Module 3/Implement a Library Management Using a HashMap/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        Library cityLibrary = new Library();
+
+        Book book1 = new Book("The Hobbit", "J.R.R. Tolkien");
+        Book book2 = new Book("1984", "George Orwell");
+        Book book3 = new Book("Dune", "Frank Herbert");
+
+        cityLibrary.addBook("978-0547928227", book1);
+        cityLibrary.addBook("978-0451524935", book2);
+        cityLibrary.addBook("978-0441172719", book3);
+
+        cityLibrary.displayBooks();
+
+        System.out.println("\n--- Fetching a specific book ---");
+        Book foundBook = cityLibrary.getBook("978-0451524935");
+        System.out.println("Found: " + foundBook.toString());
+
+        System.out.println("\n--- After removing a book ---");
+        cityLibrary.removeBook("978-0441172719");
+        cityLibrary.displayBooks();
+    }
+}
