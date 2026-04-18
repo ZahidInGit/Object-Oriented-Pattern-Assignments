c1b99be3cc6f4dbc5e5175b7d9b7242fb0aadc20
ZahidInGit <it24012@mbstu.ac.bd>
2026-04-18 20:48:59 +0600

    Create TextReader.java

---
+++ OOP course modules/Module 4/Read a text file/TextReader.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextReader {
+    
+    public void readFile(String filePath) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            System.out.println("Details: " + e.getMessage());
+        }
+    }
+}
